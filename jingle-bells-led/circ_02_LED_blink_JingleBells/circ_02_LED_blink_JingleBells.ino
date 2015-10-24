7c4d38f51aa477db5095680721810210434bc9e9
geekyhybrid <geekyhybrid@gmail.com>
2015-10-24 19:59:58 -0400

    Complete jingle-bell LED sketch.

--- jingle-bells-led/circ_02_LED_blink_JingleBells/circ_02_LED_blink_JingleBells.ino
+++ jingle-bells-led/circ_02_LED_blink_JingleBells/circ_02_LED_blink_JingleBells.ino
@@ -16,10 +16,14 @@ void loop()
 
 void jingleBellsLoop()
 {
-  //int delayTime = 500;
   tripletHandler();
   tripletHandler();
   quintetHandler();
+  mainBody();
+  tripletHandler();
+  tripletHandler();
+  quintetHandler();
+  finalBody();
 }
 
 void tripletHandler()
@@ -44,7 +48,7 @@ void tripletHandler()
 void quintetHandler()
 {
   int shortDelay = 300;
-  int longDelay = 800;
+  int longDelay = 900;
   
   for(int j = 0; j < 4; j++)
   {
@@ -59,3 +63,120 @@ void quintetHandler()
   digitalWrite(ledPins[4], LOW);
   delay(longDelay);
 }
+
+void mainBody()
+{
+  int veryShortDelay = 150;
+  int shortDelay = 300;
+  int longDelay = 600;
+  int veryLongDelay = 900;
+
+  for(int i = 0; i < 3; i++)
+  {
+    digitalWrite(ledPins[i], HIGH);
+    delay(shortDelay);
+    digitalWrite(ledPins[i], LOW);
+    delay(shortDelay);
+  }
+
+  for(int i = 3; i < 5; i++)
+  {
+    digitalWrite(ledPins[i], HIGH);
+    delay(veryShortDelay);
+    digitalWrite(ledPins[i], LOW);
+    delay(veryShortDelay);
+  }
+
+  for(int i = 5; i < 8; i++)
+  {
+    digitalWrite(ledPins[i], HIGH);
+    delay(shortDelay);
+    digitalWrite(ledPins[i], LOW);
+    delay(shortDelay);
+  }
+
+  for(int i = 0; i < 2; i++)
+  {
+    digitalWrite(ledPins[i], HIGH);
+    delay(veryShortDelay);
+    digitalWrite(ledPins[i], LOW);
+    delay(veryShortDelay);
+  }
+
+  for(int i = 2; i < 6; i++)
+  {
+    digitalWrite(ledPins[i], HIGH);
+    delay(shortDelay);
+    digitalWrite(ledPins[i], LOW);
+    delay(shortDelay);
+  }
+
+  for(int i = 6; i < 8; i++)
+  {
+    digitalWrite(ledPins[i], HIGH);
+    delay(longDelay);
+    digitalWrite(ledPins[i], LOW);
+    delay(longDelay);
+  }
+
+}
+
+void finalBody()
+{
+  int veryShortDelay = 150;
+  int shortDelay = 300;
+  int longDelay = 600;
+  int veryLongDelay = 900;
+
+  for(int i = 0; i < 3; i++)
+  {
+    digitalWrite(ledPins[i], HIGH);
+    delay(shortDelay);
+    digitalWrite(ledPins[i], LOW);
+    delay(shortDelay);
+  }
+
+  for(int i = 3; i < 5; i++)
+  {
+    digitalWrite(ledPins[i], HIGH);
+    delay(veryShortDelay);
+    digitalWrite(ledPins[i], LOW);
+    delay(veryShortDelay);
+  }
+
+  for(int i = 5; i < 8; i++)
+  {
+    digitalWrite(ledPins[i], HIGH);
+    delay(shortDelay);
+    digitalWrite(ledPins[i], LOW);
+    delay(shortDelay);
+  }
+
+  for(int i = 0; i < 2; i++)
+  {
+    digitalWrite(ledPins[i], HIGH);
+    delay(veryShortDelay);
+    digitalWrite(ledPins[i], LOW);
+    delay(veryShortDelay);
+  }
+
+  for(int i = 2; i < 6; i++)
+  {
+    digitalWrite(ledPins[i], HIGH);
+    delay(shortDelay);
+    digitalWrite(ledPins[i], LOW);
+    delay(shortDelay);
+  }
+  
+  digitalWrite(ledPins[6], HIGH);
+  delay(longDelay);
+  digitalWrite(ledPins[6], LOW);
+  delay(longDelay);
+
+  digitalWrite(ledPins[7], HIGH);
+  delay(shortDelay);
+  digitalWrite(ledPins[7], LOW);
+  delay(shortDelay);
+  
+}
+
